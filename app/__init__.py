7142c8ad55c4c3d9f4f24c43893552ba3b87a152
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-06 16:33:45 +0300

    created a db object of class SQLAlchemy and initiated it in the app

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import DevConfig
 from config import config_options
 
+
+db=SQLAlchemy()
 def create_app(config_name):
     app= Flask(__name__)
 
@@ -11,6 +13,8 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    db.init_app(app)
 
     return app 
 
--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config:
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + pitches
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    SECRET_KEY=os.environ.get('SECRET_KEY')
+    #SECRET_KEY=os.environ.get('SECRET_KEY')
     def init_app(app):
         pass 
 
