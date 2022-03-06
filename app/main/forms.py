bbeb77094a53b7a9de8912c863a474668ac5d706
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-06 12:06:46 +0300

    created a profile update class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,7 @@
 from flask_wtf import FlaskForm 
 from wtforms import StringField, SelectField, TextAreaField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
+
+class ProfileUpdate(FlaskForm):
+    profile_bio=TextAreaField('Tell us about yourself.', validators=[DataRequired])
+    submit=StringField('Save')
