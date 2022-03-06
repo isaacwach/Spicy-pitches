ada9c9c07025e1effe814c40fbe09d591e4df986
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-06 14:56:08 +0300

    created a pitchform class

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,5 +3,10 @@ from wtforms import StringField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class ProfileUpdate(FlaskForm):
-    profile_bio=TextAreaField('Tell us about yourself.', validators=[DataRequired])
+    profile_bio=TextAreaField('Tell us about yourself.', validators=[DataRequired()])
     submit=StringField('Save')
+
+class PitchForm(FlaskForm):
+    title=TextField('Title', validators=[DataRequired()])
+    category=TextField('Category', categories=[('Motivation', 'Motivation'), ('Jokes', 'Jokes'), ('Education', 'Education')], validators=[DataRequired()])
+    post= TextAreaField('Enter pitch', validators=[DataRequired()])
