7ea56d0c7121b383b086ad47d8112e5b87a1693e
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-06 19:24:19 +0300

    created a signup route

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,3 +6,13 @@ from .forms import RegForm,LoginForm
 from ..models import User
 from .. import db
 from ..email import mail_message
+
+@auth.route('/signup', methods = ["GET","POST"])
+def signup():
+    form = RegForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        user.save_u()
+        mail_message("Welcome to Pitch-World","email/welcome_user",user.email,user=user)
+        return redirect(url_for('auth.login'))
+    return render_template('auth/signup.html', r_form = form)
