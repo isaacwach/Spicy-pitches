cc41017f9cff8ffc6d36724f40bdf6e30825e458
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-06 18:10:59 +0300

    createda proprty decorator to define methods to save and delete users

--- app/models.py
+++ app/models.py
@@ -12,3 +12,25 @@ class User(db.Model):
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
     downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.secure_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.secure_password,password) 
+    
+    def save_u(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return f'User {self.username}'
