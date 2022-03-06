ca9f7ac5b2aae189b09e66c90608d2ed79db7422
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-06 17:03:53 +0300

    imported LoginManager class, created its instance and initiated it

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,11 @@ from flask import Flask
 from config import DevConfig
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 db=SQLAlchemy()
 def create_app(config_name):
@@ -15,6 +19,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    login_manager.init_app(app)
     db.init_app(app)
 
     return app 
