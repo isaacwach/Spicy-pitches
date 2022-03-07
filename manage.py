ef472241622972d40655efff044ad8e21475f24f
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-07 16:11:15 +0300

    renamed app.py to manage. py and imported User class into it. Also, imported managers class and instanciated it

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 from app import create_app
+from app.models import User
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 
 app= create_app('development')
+manager = Manager(app)
 
 @manager.shell
 def make_shell_context():
