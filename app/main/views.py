e83c1a85c2bc57541aa58e496e723fafa32d984b
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-07 08:32:06 +0300

    removed bugs

--- app/main/views.py
+++ app/main/views.py
@@ -3,9 +3,10 @@ from . import main
 
 @main.route('/')
 def index():
-    pitches = Pitch.query.all()
-    motivation = Pitch.query.filter_by(category = 'Motivation').all() 
-    jokes = Pitch.query.filter_by(category = 'Jokes').all()
-    education = Pitch.query.filter_by(category = 'Education').all()
+    # pitches = Pitch.query.all()
+    # motivation = Pitch.query.filter_by(category = 'Motivation').all() 
+    # jokes = Pitch.query.filter_by(category = 'Jokes').all()
+    # education = Pitch.query.filter_by(category = 'Education').all()
 
-    return render_template('index.html', pitches=pitches, education=education, motivation=motivation, jokes=jokes)
+    # return render_template('index.html', pitches=pitches, education=education, motivation=motivation, jokes=jokes)
+    return "Hellooooo"
