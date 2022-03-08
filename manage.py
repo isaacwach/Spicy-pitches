fb1ee896abbcbb55bbcc2e48136dce7ae9c47c63
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-08 14:21:23 +0300

    downloaded flask-migrate extention to version 2.7.0

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 
   
 from flask_script import Manager, Server
-#from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 from app.models import User
 
 app = create_app('development')
 
 manager = Manager(app)
-#migrate = Migrate(app,db)
+migrate = Migrate(app,db)
 
-#manager.add_command('db')
+manager.add_command('db', MigrateCommand)
 manager.add_command('run',Server(use_debugger=True))
 
 
