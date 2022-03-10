8a4a3cf76a482c4a6973e34decdeb45b90aafee3
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-10 09:09:22 +0300

    db initialized locally and revision file created

---
+++ migrations/versions/f895eee0bcaa_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: f895eee0bcaa
+Revises: 
+Create Date: 2022-03-10 09:08:53.713154
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f895eee0bcaa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
